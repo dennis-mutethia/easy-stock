2bb2954660e6a76ff02790c9d2340a5a3c4d719b
Dennis Mutethia Muga <10231071+dennis-mutethia@users.noreply.github.com>
2025-04-12 21:12:38 +0300

    Update demo_stock.py

--- demo_stock.py
+++ demo_stock.py
@@ -89,7 +89,6 @@ class DemoSTock():
     def update(self, stock_date, shop_id):
         stocks = self.fetch(stock_date, shop_id)
         stocks = random.sample(stocks, min(15, len(stocks)))
-        stocks = random.sample(stocks, 15)
         self.db.ensure_connection()
         for stock in stocks:
             opening = stock.yesterday_opening + stock.yesterday_additions
