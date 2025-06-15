7fc066581fb3e53941700039842800c21d3dfffe
Dennis Mutethia Muga <10231071+dennis-mutethia@users.noreply.github.com>
2025-06-15 13:39:57 +0300

    Update stock_take.py

--- utils/stock/stock_take.py
+++ utils/stock/stock_take.py
@@ -57,7 +57,10 @@ class StockTake():
             WHERE shop_id = %s
         ),
         all_stock AS(
-            SELECT id, stock_date, product_id, COALESCE(opening,0) AS opening, COALESCE(additions,0) AS additions, selling_price, purchase_price
+            SELECT id, stock_date, product_id, 
+            CASE WHEN opening IS NAN THEN 0 ELSE opening END AS opening,
+            CASE WHEN additions IS NAN THEN 0 ELSE additions END AS additions,
+            selling_price, purchase_price
             FROM stock 
             WHERE shop_id = %s
         ),  
