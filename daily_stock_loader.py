7b66d7b30b3220301ecb9c5185967c1b9d1fb88a
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-07-06 12:41:39 +0300

    DailyStockLoader

---
+++ daily_stock_loader.py
@@ -0,0 +1,53 @@
+import pytz
+from datetime import datetime
+
+from utils.db import Db
+
+class DailyStockLoader():
+    def __init__(self): 
+        self.db = Db()
+                    
+    def load(self, stock_date):
+        self.db.create_current_month_partition()
+        
+        self.db.ensure_connection()
+        with self.db.conn.cursor() as cursor:     
+            query = """         
+            WITH products AS (
+                SELECT id, shop_id, name, category_id, purchase_price, selling_price
+                FROM products 
+            ),
+            yesterday AS (
+                SELECT product_id, purchase_price, selling_price, opening, additions
+                FROM stock
+                WHERE DATE(stock_date) = DATE(%s) - 1
+            ),
+            today AS (
+                SELECT 
+                    DATE(%s) AS stock_date, 
+                    COALESCE(yesterday.product_id, products.id) AS product_id, 
+                    COALESCE(yesterday.purchase_price, products.purchase_price) AS purchase_price,
+                    COALESCE(yesterday.selling_price, products.selling_price) AS selling_price,
+                    COALESCE((yesterday.opening+yesterday.additions), 0) AS opening,
+                    0 AS additions,
+                    products.shop_id AS shop_id, 
+                    CURRENT_TIMESTAMP AT TIME ZONE 'Africa/Nairobi' AS created_at, 
+                    0 AS created_by              
+                FROM products
+                LEFT JOIN yesterday ON yesterday.product_id = products.id
+            )
+            INSERT INTO stock (stock_date, product_id, purchase_price, selling_price, opening, additions, shop_id, created_at, created_by) 
+            SELECT * FROM today
+            ON CONFLICT (stock_date, product_id, shop_id) DO NOTHING
+            """
+            params = [stock_date, stock_date]
+     
+            cursor.execute(query, tuple(params))
+            self.db.conn.commit()
+    
+    def __call__(self):
+        current_date = datetime.now(pytz.timezone("Africa/Nairobi")).strftime('%Y-%m-%d')
+        stock_date = current_date 
+        self.load(stock_date)
+
+DailyStockLoader()()
--- demo_stock.py
+++ demo_stock.py
@@ -7,43 +7,7 @@ from utils.entities import Stock
 class DemoSTock():
     def __init__(self): 
         self.db = Db()
-                    
-    def load(self, stock_date, shop_id):
-        self.db.create_current_month_partition()
-        
-        self.db.ensure_connection()
-        with self.db.conn.cursor() as cursor:     
-            query = """         
-            WITH products AS (
-                SELECT id, name, category_id, purchase_price, selling_price
-                FROM products 
-                WHERE shop_id = %s
-            ),
-            yesterday AS (
-                SELECT product_id, purchase_price, selling_price, opening, additions
-                FROM stock
-                WHERE DATE(stock_date) = DATE(%s) - 1
-            ),
-            today AS (
-                SELECT DATE(%s) AS stock_date, 
-                    COALESCE(yesterday.product_id, products.id) AS product_id, 
-                    COALESCE(yesterday.purchase_price, products.purchase_price) AS purchase_price,
-                    COALESCE(yesterday.selling_price, products.selling_price) AS selling_price,
-                    COALESCE((yesterday.opening+yesterday.additions), 0) AS opening,
-                    0 AS additions,
-                    %s AS shop_id, CURRENT_TIMESTAMP AT TIME ZONE 'Africa/Nairobi' AS created_at, %s AS created_by              
-                FROM products
-                LEFT JOIN yesterday ON yesterday.product_id = products.id
-            )
-            INSERT INTO stock (stock_date, product_id, purchase_price, selling_price, opening, additions, shop_id, created_at, created_by) 
-            SELECT * FROM today
-            ON CONFLICT (stock_date, product_id, shop_id) DO NOTHING
-            """
-            params = [shop_id, stock_date, stock_date, shop_id, 0]
-     
-            cursor.execute(query, tuple(params))
-            self.db.conn.commit()
-    
+
     def fetch(self, stock_date, shop_id):
         self.db.ensure_connection()
     
@@ -119,7 +83,6 @@ class DemoSTock():
         current_date = datetime.now(pytz.timezone("Africa/Nairobi")).strftime('%Y-%m-%d')
         stock_date = current_date 
         for shop_id in shop_ids:  
-            self.load(stock_date, shop_id)
             self.update(stock_date, shop_id)
 
 DemoSTock()()
